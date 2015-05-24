bbf43a238a32c8a33549a49c0809e5c73bad169d
justin massena <justin@massena.com>
2015-05-24 07:20:31 -0700

    working on automating the distribution build

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,21 @@
 // John Papa's example in his module project
 // And while your at it have it minify to a build directory
 
+var paths = {
+  css: './src/client/app/content/*.css',
+  img: './src/client/app/content/img/*.*',
+  html: './src/client/app/**/*.html',
+  indexHtml: 'src/client/app/index.html',
+  js: './src/client/app/**/*.js',
+  fileNames: {
+    templateCache: 'template.js'
+  },
+  // server: {
+  //
+  // },
+  build: './dist/'
+};
+
 // packages
 var gulp = require('gulp'),
   gutil = require('gulp-util'),
@@ -15,13 +30,76 @@ var gulp = require('gulp'),
   browserSync = require('browser-sync'),
   nodemon = require('gulp-nodemon'),
   mocha = require('gulp-spawn-mocha'),
-  runSequence = require('run-sequence');
+  runSequence = require('run-sequence'),
+  del = require('del'),
+  angularTemplatecache = require('gulp-angular-templatecache'),
+  inject = require('gulp-inject');
+
+gulp.task('css', ['clean'], function () {
+  // copy html from angular app to dist/public
+  return gulp
+    .src(paths.css)
+    .pipe(gulp.dest(paths.build + 'content/'));
+
+});
+gulp.task('js', ['clean'], function () {
+  // copy html from angular app to dist/public
+  return gulp
+    .src(paths.js)
+    .pipe(gulp.dest(paths.build));
+
+});
+gulp.task('img', ['clean'], function () {
+  // copy html from angular app to dist/public
+  return gulp
+    .src(paths.img)
+    .pipe(gulp.dest(paths.build + 'content/img/'));
+
+});
+
+gulp.task('inject', ['templateCache'], function () {
+  // copy html from angular app to dist/public
+
+  return gulp
+    .src(paths.indexHtml)
+    .pipe(inject(gulp.src(paths.fileNames.templateCache, {
+      cwd: paths.build
+    })))
+    // .pipe(inject(gulp.src(paths.fileNames.templateCache)))
+    .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('templateCache', ['clean'], function () {
+  // log('Creating an AngularJS $templateCache');
+
+  return gulp
+    .src(['src/client/app/**/*.html', '!' + paths.indexHtml])
+    // .pipe(plug.bytediff.start())
+    // .pipe(plug.minifyHtml({
+    //     empty: true
+    // }))
+    // .pipe(plug.bytediff.stop(bytediffFormatter))
+    .pipe(angularTemplatecache(paths.fileNames.templateCache, {
+      module: 'app',
+      standalone: false,
+      root: 'app/'
+    }))
+    .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('clean', function (cb) {
+  return del(['dist'],
+    cb);
+});
+
+gulp.task('build', ['templateCache', 'css', 'img', 'js', 'inject']);
 
 // jshint
 gulp.task('jshint', function () {
   return gulp.src('src/client/**/*.js')
     .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('jshint-watch', function () {
@@ -63,6 +141,9 @@ gulp.task('mocha-watch', function () {
 gulp.task('karma-watch', function () {
   return gulp.watch(['src/client/**/*.js'], ['karma']);
 });
+
+// build
+// copy all html,js,css
 
 // TODO: need to make sure the node server is running before karma
 gulp.task('test', function (cb) {
